d02cbd4776bf8ba0a093c418c6c8aa7c136c80ba
Salavat <asnsalavat@gmail.com>
2022-02-01 00:21:58 +0100

    test commit

--- app/src/main/java/com/example/stocks/MainActivity.java
+++ app/src/main/java/com/example/stocks/MainActivity.java
@@ -25,29 +25,13 @@ public class MainActivity extends AppCompatActivity {
     private static final int SIGN_IN_CODE = 1;
     TextView text;
     Button button;
-    SignInButton mSignInButton;
-    GoogleSignInClient mGoogleSignInClient;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        GoogleSignInOptions gso = new GoogleSignInOptions
-                .Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken("558262318409-ssodom6fmpgfb5r6h63o9q81ktl8orto.apps.googleusercontent.com")
-                .requestEmail()
-                .build();
-        Log.d("Error", "hello");
-
-        try {
-            mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
-        } catch (Exception e) {
-            Log.e("Error", "e", e);
-        }
-
         text = findViewById(R.id.text);
         button = findViewById(R.id.button);
-        mSignInButton = findViewById(R.id.sign_in_button);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -56,27 +40,14 @@ public class MainActivity extends AppCompatActivity {
                 connectToDatabase();
             }
         });
-
-        mSignInButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                signIn();
-            }
-        });
     }
 
     private void signIn() {
-        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
-        startActivityForResult(signInIntent, SIGN_IN_CODE);
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-
-        GoogleSignInAccount account = null;
-        account = GoogleSignIn.getLastSignedInAccount(MainActivity.this);
-        if (account != null) Toast.makeText(this, "roe[gerogk[gv", Toast.LENGTH_SHORT).show();
     }
 
     private void connectToDatabase() {
@@ -91,18 +62,10 @@ public class MainActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
 
         if (requestCode == SIGN_IN_CODE) {
-            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
-            handleSignInResult(task);
         }
     }
 
     private void handleSignInResult(Task<GoogleSignInAccount> task) {
-        try {
-            GoogleSignInAccount account = task.getResult(ApiException.class);
-            Toast.makeText(this, "Congrats!", Toast.LENGTH_SHORT).show();
-        } catch (ApiException e) {
-            e.printStackTrace();
-            Log.w("Error", "signInResult:failed code=" + e.getStatusCode());
-        }
+
     }
 }
