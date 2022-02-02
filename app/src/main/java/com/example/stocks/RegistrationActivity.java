0f99dd67539381ce4205f467a75fdb93bcb5c96b
Salavat <asnsalavat@gmail.com>
2022-02-03 00:39:39 +0100

    Deleted profile photo initializing in registration page

--- app/src/main/java/com/example/stocks/RegistrationActivity.java
+++ app/src/main/java/com/example/stocks/RegistrationActivity.java
@@ -5,6 +5,7 @@ import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -12,29 +13,35 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
 import java.io.IOException;
-import java.util.concurrent.atomic.AtomicBoolean;
 
 public class RegistrationActivity extends AppCompatActivity {
     private static final int SIGN_IN_CODE = 1;
-    TextView mRegistrationTextView, mCustomizeYourProfilePhotoTextView;
+    TextView mRegistrationTextView;
     EditText mLoginEditText, mPasswordEditText, mNameEditText;
-    ImageView mProfilePhotoImageView;
     Button mSignUp;
 
-
-    // for saving profile image
-    Uri uri;
-    private final int PICK_IMAGE_REQUEST = 22;
+    String profileImageLink = "https://firebasestorage.googleapis.com/v0/b/stocks-95f7e.appspot.com/o/images%2Fuser.png?alt=media&token=99b47971-03f1-4ea8-b36b-59768692e3c7";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,18 +52,10 @@ public class RegistrationActivity extends AppCompatActivity {
         mLoginEditText = findViewById(R.id.login_registration);
         mPasswordEditText = findViewById(R.id.password_registration);
         mNameEditText = findViewById(R.id.name_registration);
-        mCustomizeYourProfilePhotoTextView = findViewById(R.id.customize_your_profile_photo_text);
-        mProfilePhotoImageView = findViewById(R.id.profile_photo_registration);
         mSignUp = findViewById(R.id.sign_up_button);
 
         mSignUp.setOnClickListener(view -> signUp());
 
-        mProfilePhotoImageView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                selectImage();
-            }
-        });
     }
 
     @Override
@@ -72,7 +71,6 @@ public class RegistrationActivity extends AppCompatActivity {
         String login = mLoginEditText.getText().toString();
         String password = mPasswordEditText.getText().toString();
         String name = mNameEditText.getText().toString();
-        String profileImageLink = getProfileImageLink();
 
         if (login.isEmpty()) {
             Toast.makeText(this, "Login is empty.", Toast.LENGTH_SHORT).show();
@@ -94,63 +92,6 @@ public class RegistrationActivity extends AppCompatActivity {
 
         userRef.setValue(user);
         Toast.makeText(this, "Success!", Toast.LENGTH_SHORT).show();
-    }
-
-    String uploadedImageUrl = "";
-    private String getProfileImageLink() {
-        StorageReference storageRef = FirebaseStorage.getInstance().getReference();
-
-        String link = uri == null ? "user.png" : uri.getLastPathSegment();
-
-        StorageReference uploadImageRef = storageRef.child("images/"+ link);
-
-        if (uri == null) uri = Uri.parse("android.resource://com.example.stocks/" + R.drawable.user);
-
-        UploadTask uploadTask = uploadImageRef.putFile(uri);
-
-        // Как сделать, чтобы асинхронный uploadedImageUrl сначала получал результат, а потом его возвращал?
-        uploadTask.addOnSuccessListener(taskSnapshot -> taskSnapshot.getStorage().getDownloadUrl().addOnCompleteListener(task -> {
-                uploadedImageUrl = task.getResult().toString();
-        })).addOnFailureListener(e -> {});
-
-        return uploadedImageUrl;
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
-            uri = data.getData();
-            try {
-
-                Bitmap bitmap = MediaStore
-                        .Images
-                        .Media
-                        .getBitmap(
-                                getContentResolver(),
-                                uri);
-                mProfilePhotoImageView.setImageBitmap(bitmap);
-            }
-
-            catch (IOException e) {
-                // Log the exception
-                e.printStackTrace();
-            }
-        } else {
-            Toast.makeText(this, "hahaahhaa", Toast.LENGTH_SHORT).show();
-        }
-    }
-
-    private void selectImage() {
-        Intent intent = new Intent();
-        intent.setType("image/*");
-        intent.setAction(Intent.ACTION_GET_CONTENT);
-        startActivityForResult(
-                Intent.createChooser(
-                        intent,
-                        "Select Image from here..."),
-                PICK_IMAGE_REQUEST);
     }
 
     private class User {
