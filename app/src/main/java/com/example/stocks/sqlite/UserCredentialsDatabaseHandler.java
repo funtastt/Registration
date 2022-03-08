469a0989ed0f4c8e3f16acd8734e02f99908c017
Salavat <asnsalavat@gmail.com>
2022-03-08 22:32:37 +0100

    Added multicurrency to SQLite

--- app/src/main/java/com/example/stocks/sqlite/UserCredentialsDatabaseHandler.java
+++ app/src/main/java/com/example/stocks/sqlite/UserCredentialsDatabaseHandler.java
@@ -7,10 +7,19 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 import androidx.annotation.Nullable;
 
 import com.example.stocks.User;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 public class UserCredentialsDatabaseHandler extends SQLiteOpenHelper {
     private static final String TABLE_USER_CREDENTIALS = "user_credentials";
@@ -21,6 +30,7 @@ public class UserCredentialsDatabaseHandler extends SQLiteOpenHelper {
     private static final String KEY_MAIL = "mail";
     private static final String KEY_BIRTHDAY_DATE = "birthday_date";
     private static final String KEY_PROFILE_PHOTO = "profile_photo";
+    private static final String KEY_BALANCES = "balances";
 
     public UserCredentialsDatabaseHandler(@Nullable Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -35,6 +45,7 @@ public class UserCredentialsDatabaseHandler extends SQLiteOpenHelper {
                 KEY_NAME + " TEXT, " +
                 KEY_MAIL + " TEXT, " +
                 KEY_BIRTHDAY_DATE + " LONG, " +
+                KEY_BALANCES + " TEXT, " +
                 KEY_PROFILE_PHOTO + " TEXT)");
     }
 
@@ -58,19 +69,41 @@ public class UserCredentialsDatabaseHandler extends SQLiteOpenHelper {
         values.put(KEY_NAME, user.getName());
         values.put(KEY_MAIL, user.getMail());
         values.put(KEY_BIRTHDAY_DATE, user.getBirthdayDate());
+        values.put(KEY_BALANCES, mapToString(user.getBalances()));
         values.put(KEY_PROFILE_PHOTO, user.getProfilePhotoLink());
         db.insert(TABLE_USER_CREDENTIALS, null, values);
         db.close();
     }
 
+    private String mapToString(HashMap<String, Double> balances) {
+        return new JSONObject(balances).toString();
+    }
+
     public User getUser() {
         SQLiteDatabase db = this.getReadableDatabase();
-        Cursor cursor = db.query(TABLE_USER_CREDENTIALS, new String[] {KEY_LOGIN, KEY_PASSWORD, KEY_NAME, KEY_MAIL, KEY_BIRTHDAY_DATE, KEY_PROFILE_PHOTO, PRIMARY_KEY_ID }, null,
+        Cursor cursor = db.query(TABLE_USER_CREDENTIALS, new String[] {KEY_LOGIN, KEY_PASSWORD, KEY_NAME, KEY_MAIL, KEY_BIRTHDAY_DATE, KEY_PROFILE_PHOTO, PRIMARY_KEY_ID, KEY_BALANCES }, null,
                 null, null, null, null, String.valueOf(1));
         if (cursor != null)
             cursor.moveToFirst();
         User user = new User(cursor.getString(0), cursor.getString(1), cursor.getString(2), cursor.getString(3), Long.parseLong(cursor.getString(4)), cursor.getString(5));
         user.setUserId(Integer.parseInt(cursor.getString(6)));
+        String balancesString = cursor.getString(7);
+        try {
+            JSONObject object = new JSONObject(balancesString);
+            Iterator<String> nameItr = object.keys();
+            Map<String, Double> outMap = new HashMap<>();
+            while(nameItr.hasNext()) {
+                String name = nameItr.next();
+                outMap.put(name, object.getDouble(name));
+            }
+
+            for (Map.Entry<String, Double> map : outMap.entrySet()) {
+                Log.d("Error", map.getKey() + ", " + map.getValue());
+            }
+
+            } catch (JSONException e) {
+            e.printStackTrace();
+        }
         return user;
     }
 
@@ -82,6 +115,7 @@ public class UserCredentialsDatabaseHandler extends SQLiteOpenHelper {
         values.put(KEY_NAME, user.getName());
         values.put(KEY_MAIL, user.getMail());
         values.put(KEY_BIRTHDAY_DATE, user.getBirthdayDate());
+        values.put(KEY_BALANCES, mapToString(user.getBalances()));
         values.put(KEY_PROFILE_PHOTO, user.getProfilePhotoLink());
         return database.update(TABLE_USER_CREDENTIALS, values, PRIMARY_KEY_ID + " = ?", new String[]{String.valueOf(user.getUserId())});
     }
